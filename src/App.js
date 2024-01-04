0457d35d1a2aef5d7ed4aa6e238fb0e70f65ff4c
Nazasophie <chinazasundaygoodness@gmail.com>
2024-01-04 02:58:29 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Form from "./components/Form";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-center">Let's Chinaza create a unique Resume for You!</h1>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
